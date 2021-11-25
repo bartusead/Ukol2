aaceaeb09d379d3765844d960b8adcc7e18c5a00
Adam Bartůšek <bartusek.a@seznam.cz>
2021-11-25 10:42:41 +0100

    soucty

--- ukol2.py
+++ ukol2.py
@@ -9,9 +9,29 @@ with open("data.csv", encoding="utf8") as csvinfile,\
            
 # Chyby - chybí soubor, nemám práva, není .csv...
 
-    soucet_prutoku = 0
+    soucet_prutoku = 0 
     cislo_radku = 0
     zbytek = 0
+
+    for row in reader:
+        if cislo_radku % 7 == 0:
+            prvni_den = row
+        
+        try:
+            soucet_prutoku = soucet_prutoku + float(row[5])
+            zbytek = zbytek + 1
+            print(f"soucet prutoku je {soucet_prutoku} ")
+            print(zbytek)
+        except ValueError:
+            pass
+
+        
+
+            
+        
+        
+
+
        
     
     

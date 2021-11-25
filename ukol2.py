a9d453d0e012438d277118dbd8cfed131f647b03
Adam Bartůšek <bartusek.a@seznam.cz>
2021-11-25 10:12:54 +0100

    promenne

--- ukol2.py
+++ ukol2.py
@@ -1,6 +1,20 @@
 import csv
 
 with open("data.csv", encoding="utf8") as csvinfile,\
-        open("vystup_7dni.csv", "w", encoding="utf8") as csvoutfile:
+       open("vystup_7dni.csv", "w", encoding="utf8") as csvoutfile_tyden:
     reader = csv.reader(csvinfile, delimiter = ",")
-    writer = csv.writer(csvoutfile)
+    writer_tyden = csv.writer(csvoutfile_tyden)
+    
+    
+           
+# Chyby - chybí soubor, nemám práva, není .csv...
+
+    soucet_prutoku = 0
+    cislo_radku = 0
+    zbytek = 0
+       
+    
+    
+   
+
+       

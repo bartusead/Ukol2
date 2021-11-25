c573106be9462b8981fce0a64baba045a2bef7ff
Adam Bartůšek <bartusek.a@seznam.cz>
2021-11-25 11:26:43 +0100

    Zapis tydne

--- ukol2.py
+++ ukol2.py
@@ -14,16 +14,26 @@ with open("data.csv", encoding="utf8") as csvinfile,\
     zbytek = 0
 
     for row in reader:
+        if float(row[5]) <= 0:
+            print(f"Dne {row[4]}.{row[3]}.{row[2]} došlo k nulovému či zápornému průtoku!")
         if cislo_radku % 7 == 0:
             prvni_den = row
         
         try:
             soucet_prutoku = soucet_prutoku + float(row[5])
             zbytek = zbytek + 1
-            print(f"soucet prutoku je {soucet_prutoku} ")
-            print(zbytek)
         except ValueError:
-            pass
+            print("Průtok není v čísleném formátu!")
+
+        if cislo_radku % 7 == 6:
+            avg_prutok = soucet_prutoku / 7
+            prvni_den[5] = f"{avg_prutok:.4f}"
+            writer_tyden.writerow(prvni_den)
+            soucet_prutoku = 0 
+            avg_prutok = 0
+            zbytek = 0
+        cislo_radku += 1
+        
 
         
 

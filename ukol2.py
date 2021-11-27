b3ae2c3e7897f7718982273187fccd6d845e612c
Adam Bartůšek <bartusek.a@seznam.cz>
2021-11-27 21:04:13 +0100

    Kontrola děr + max a min

--- ukol2.py
+++ ukol2.py
@@ -20,10 +20,7 @@ with open("vstup.csv", encoding="utf8") as csvinfile,\
         open("vystup_7dni.csv", "w", encoding="utf8") as csvoutfile_tyden:
         reader = csv.reader(csvinfile, delimiter = ",")
         writer_tyden = csv.writer(csvoutfile_tyden)
-        # Nastaví první řádek do proměnné poc_rok (pak se z něj zjišťuje první rok v souboru) a přeskočí opět na začátek souboru
-        poc_rok = next(reader)
-        csvinfile.seek(0)
-        
+
         # Definice proměnných
         soucet_prutoku = 0 
         cislo_radku = 0
@@ -54,7 +51,6 @@ with open("vstup.csv", encoding="utf8") as csvinfile,\
                 prvni_den[5] = f"{avg_prutok:.4f}"
                 writer_tyden.writerow(prvni_den)
                 soucet_prutoku = 0 
-                avg_prutok = 0
                 zbytek = 0
             cislo_radku += 1
         
@@ -106,27 +102,64 @@ with open("vstup.csv", encoding="utf8") as csvinfile,\
                 avg_prutok_rok = soucet_prutoku_rok / zbytek_rok
                 prvni_den_rok[5] = f" {avg_prutok_rok:.4f}"
                 writer_rok.writerow(prvni_den_rok)
+
+# Maximální a minimální průtok
+# Načtení souboru
+
+with open("vstup.csv", encoding="utf-8") as csvinfile:
+    reader = csv.reader(csvinfile, delimiter = ",")
+
+    for row in reader:
+        # Stanovení max. a min. průtoku podle prvního řádku
+        if reader.line_num == 1:
+            prutok_max_radek = row
+            prutok_min_radek = row
+            prutok_max = float(prutok_max_radek[5])
+            prutok_min = float(prutok_min_radek[5])
+        soucasny_prutok = float(row[5])
+        # Program vždy porovná současný průtok s max. a min. průtokem a pokud je větší (menší), přepíše proměnnou max a min
+        if soucasny_prutok > prutok_max:
+            prutok_max_radek = row
+            prutok_max = soucasny_prutok
+        if soucasny_prutok < prutok_min:
+            prutok_min_radek = row
+            prutok_min = soucasny_prutok
+    print(f"Dne {prutok_max_radek[4]}.{prutok_max_radek[3]}.{prutok_max_radek[2]} byl detekován maximální průtok, a to {prutok_max} m^3/s.")
+    print(f"Dne {prutok_min_radek[4]}.{prutok_min_radek[3]}.{prutok_min_radek[2]} byl detekován minimální průtok, a to {prutok_min} m^3/s.")
+
+# Kontrola chybějících dnů
+# Načtení souboru
+with open("vstup.csv", encoding="utf8") as csvinfile:
+            reader = csv.reader(csvinfile, delimiter = ",")
+            # Definice proměnných          
+            cislo_radku_mes = 0
+            zbytek_mesic = 0
+            for row in reader:
+                # Definice prvního měsíce
+                soucasny_mes = int(row[3])
+                if cislo_radku_mes == 0:
+                    mesic = soucasny_mes
+                try:
+                    zbytek_mesic += 1 
+                except ValueError:
+                    pass
+                
+                # Když program zjistí nový měsíc, porovná, kolik dnů měl mít měsíc a kolik jsich skutečně měl
+                if soucasny_mes != mesic:
+                    if (int(row[3]) == 5 or int(row[3]) == 7 or int(row[3]) == 10 or int(row[3]) == 12) and zbytek_mesic != 31:
+                        print(f"V měsíci {int(row[3])-1} roku {row[2]} chybí {31 - zbytek_mesic} dny")
                     
+                    elif int(row[3]) == 3 and int(row[2]) % 4 == 0 and zbytek_mesic != 30:    
+                        print(f"V měsíci {int(row[3])-1} roku {row[2]} chybí {30 - zbytek_mesic} dny")
+                    elif int(row[3]) == 3 and int(row[2]) % 4 != 0 and zbytek_mesic != 29:
+                        print(f"V měsíci {int(row[3])-1} roku {row[2]} chybí {29 - zbytek_mesic} dny")
 
-
-       
-
-
-    
-
-
-        
-
-        
-
-            
-        
-        
-
-
-       
-    
-    
-   
-
-       
+                    elif (int(row[3]) == 2 or int(row[3]) == 4 or int(row[3]) == 6 or int(row[3]) == 8 or int(row[3]) == 9 or int(row[3]) == 11) and zbytek_mesic != 32:
+                        print(f"V měsíci {int(row[3])-1} roku {row[2]} chybí {32 - zbytek_mesic} dny")
+                    
+                    elif int(row[3]) == 1 and zbytek_mesic != 32:
+                        print(f"V měsíci 12 roku {row[2]} chybí {32 - zbytek_mesic} dny")
+                    
+                    mesic = soucasny_mes
+                    zbytek_mesic = 1
+                cislo_radku_mes +=1
